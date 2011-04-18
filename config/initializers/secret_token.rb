0eed0a14e49e1779f0d43ef2100e4b3bea67357d
Ceren Temur <ceren_t@hotmail.de>
2011-04-18 15:35:15 +0200

    erstes commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ceren::Application.config.secret_token = 'af61e5c6c3bc6dcef022ec6aef1c6097d3da52c5f5d251708ca416cf4b5867fef1f34321f3b40174461cf58af020361be130872e811202be285a4ac654cb02d0'
